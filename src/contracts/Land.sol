b46434ad58aa808fee8d788be3b6c05f2b00ee79
Dhaesha M <dhaesha1@gmail.com>
2022-03-31 12:39:07 -0400

    setting up Building struct w all variables to specify name, owner + location

--- src/contracts/Land.sol
+++ src/contracts/Land.sol
@@ -9,4 +9,15 @@ contract Land is ERC721 {
     uint256 public maxSupply = 5;
     //totalSupply is default start point
     uint256 public totalSupply = 0;
+
+    struct Building {
+        string name;
+        address owner;
+        int256 posX;
+        int256 posY;
+        int256 posZ;
+        uint256 sizeX;
+        uint256 sizeY;
+        uint256 sizeZ;
+    }
 }
