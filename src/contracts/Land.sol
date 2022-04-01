75f29d8498f592c23885c60c4d2d2d1dbba4c058
Dhaesha M <dhaesha1@gmail.com>
2022-03-31 22:12:58 -0400

    setting up function to buy or mint land. set up requirements to be met before function is ran. requirements is that the land is available, the building has no owner + the correct amount of currency is being sent to function

--- src/contracts/Land.sol
+++ src/contracts/Land.sol
@@ -6,6 +6,7 @@ import  "@openzeppelin/contracts/token/ERC721/ERC721.sol"
 contract Land is ERC721 {
 
     uint256 public cost = 1 ether;
+    // max supply is # of buildings 
     uint256 public maxSupply = 5;
     //totalSupply is default start point
     uint256 public totalSupply = 0;
@@ -41,5 +42,21 @@ contract Land is ERC721 {
             Building("Vegan Restaurant", address(0x0), -10, 0, 0, 5, 25, 5)
         );
     }
+    //function is reusable code to be called whenever needed
+    //to mint/buy land
+    //public: anybody can call it
+    //payable: can send currency to function
+    function mint(uint256 _id) public payable {
+        // to make sure land is available 
+        uint256 supply = totalSupply;
+        // require checks for condition in order to run function
+        // supply must be less than maxSupply which is total available buildings 
+        require(supply <= maxSupply);
+        // make sure building owner = 0x0
+        require(buildings[_id -1].owner == address(0x0));
+        // making sure ether sent to payable function is equal/greater than to cost
+        require(msg.value >= 1ether);
+    }
+
 
 }
