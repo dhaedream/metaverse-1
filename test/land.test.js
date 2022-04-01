224e241c3b1f651037658b812b9349104e82e483
Dhaesha M <dhaesha1@gmail.com>
2022-04-01 12:15:19 -0400

    setting up test for expcted activities we want to happen with the purchase like transfering ownership + recording/updating accurate building

--- test/land.test.js
+++ test/land.test.js
@@ -5,7 +5,8 @@ require("chai").use(require("chai-as-promised")).should();
 
 // takes 2 vars, contract name
 // and the test function that will run
-contract("Land", () => {
+//owners from ganache for spending test ether
+contract("Land", ([owner1, owner2]) => {
   // we need these variables to satisfy constructor + deploy contract
   const NAME = "Dapp U Buildings";
   const SYMBOL = "DUB";
@@ -35,5 +36,46 @@ contract("Land", () => {
       //using chai keyword shoud for test
       result.should.equal(NAME);
     });
+    it("returns symbol", async () => {
+      result = await land.symbol();
+      result.should.equal(SYMBOL);
+    });
+    it("returns cost to mint/buy", async () => {
+      result = await land.cost();
+      result.toString().should.equal(COST);
+    });
+    it("returns max supply", async () => {
+      result = await land.maxSupply();
+      result.toString().should.equal("5");
+    });
+    it("returns # of buildings available", async () => {
+      result = await land.getBuildings();
+      result.length.should.equal(5);
+    });
   });
+
+  describe("Minting", () => {
+      describe("Successful purchase", () => {
+        //before each test we mint a plot of land w/ id of 1
+        // AND specify the address buying land AND cost
+          result = await land.mint(1, { from: owner1, value: COST })
+
+        // after yu mint NodeFilter, owner address should be updated 
+        it("Updates owner address", async () => {
+            result = await land.ownerOf(1)
+            result.should.equal(owner1)
+        })
+
+        // check if building is added to array 
+        it("Updates building details", async () => {
+            result = await land.getBuilding(1)
+            result.owner1.should.equal(owner1)
+        })
+      })    
+  })
+
+  describe('Failure', () => {
+      
+  })
+
 });
