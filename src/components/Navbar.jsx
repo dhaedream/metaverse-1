6cb16ddfb00679ab68ec56624f1cdc94bfa0acd8
Dhaesha M <dhaesha1@gmail.com>
2022-04-02 15:28:37 -0400

    set up navbar to display some user info by incorporating web3Handler, if no user account is detected, navbar will ask user to connect

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from "../assets/3d-logo.png"
+
+const Navbar = ({ web3Handler, account }) => {
+  return (
+    <nav className='flex-between'>
+        <a className='flex'>
+            <img src={logo} className="App-logo" alt="logo" />
+            Dapp University
+        </a>
+
+        {accoount ? (
+            <a
+            href={`${""}/address/${account}`
+            target="_blank"
+            rel="noopener noreferrer"
+            className='button'>
+                { account.slice( 0, 5 ) + '...' + account.slice( 38, 42 )}
+            </a>
+        ) : (
+            <button onClick={web3Handler} className="button">Connect Wallet</button>
+        )}
+
+    </nav>
+  )
+}
+
+export default Navbar
