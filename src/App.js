68d5e70fe86716290ceb0e3ea290d217bb3fd7cd
Dhaesha M <dhaesha1@gmail.com>
2022-04-01 21:56:35 -0400

    setting up App useStates to target + handle smart contract data + updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
 import Web3 from "web3";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="">DAPPing It !!</div>;
+  // ready to pull web3 object/s into app +
+  // using set ... to update info in app
+  const [web3, setWeb3] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [landContract, setLandContract] = useState(null);
+  const [cost, setCost] = useState(0);
+  const [buildings, setBuildings] = useState(null);
+
+  return (
+    <div className="">
+      <p>DAPPing It !!</p>
+    </div>
+  );
 }
 
 export default App;
