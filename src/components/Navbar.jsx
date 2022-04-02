7dfd03fabbf940d6c50f33de1a742c9808e9495f
Dhaesha M <dhaesha1@gmail.com>
2022-04-02 15:44:28 -0400

    navbar + web3Handeler for metamask login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import logo from "../assets/3d-logo.png"
+import React from "react";
+import logo from "../assets/3d-logo.png";
 
 const Navbar = ({ web3Handler, account }) => {
   return (
-    <nav className='flex-between'>
-        <a className='flex'>
-            <img src={logo} className="App-logo" alt="logo" />
-            Dapp University
+    <nav className="flex-between">
+      <a className="flex">
+        <img src={logo} className="App-logo" alt="logo" />
+        Dapp University
+      </a>
+
+      {accoount ? (
+        <a
+          href={`${""}/address/${account}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="button"
+        >
+          {account.slice(0, 5) + "..." + account.slice(38, 42)}
         </a>
-
-        {accoount ? (
-            <a
-            href={`${""}/address/${account}`
-            target="_blank"
-            rel="noopener noreferrer"
-            className='button'>
-                { account.slice( 0, 5 ) + '...' + account.slice( 38, 42 )}
-            </a>
-        ) : (
-            <button onClick={web3Handler} className="button">Connect Wallet</button>
-        )}
-
+      ) : (
+        <button onClick={web3Handler} className="button">
+          Connect Wallet
+        </button>
+      )}
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
